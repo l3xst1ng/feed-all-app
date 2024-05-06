f3712a426397d1566b5a758355c62cae316c55c6
l3xst1ng <howdy.martt@gmail.com>
2024-05-06 09:05:35 +0300

    Edited donate button click events and added function to display the form

--- src/index.js
+++ src/index.js
@@ -1,55 +1,59 @@
 // Function to show the selected nav section and hide the others
 
-
 const homeSection = document.getElementById("home-page");
 
 const otherSections = document.querySelectorAll(".hidden-section");
 
 const navLinks = document.querySelectorAll("nav ul li a");
 
-
 function showSection(sectionId) {
-    homeSection.style.display = "none"; // Hides the home section
-    otherSections.forEach((section) => {
-      if (section.id === sectionId) {
-        section.style.display = "block";
-      } else {
-        section.style.display = "none";
-      }
-    });
-  }
-  
-  // Displaying the home section by default
-  homeSection.style.display = "block";
-  
-  //  click event listeners to the navigation links
-  navLinks.forEach((link) => {
-    link.addEventListener("click", (event) => {
-      event.preventDefault(); // Prevents reloading - the default link behavior
-  
-      // Get the target section ID from the link's href
-      const targetSectionId = event.target.getAttribute("href").slice(1);
-  
-      if (targetSectionId === "home-page") {
-        // If the target section is the home page, show the home section
-        homeSection.style.display = "block";
-        otherSections.forEach((section) => (section.style.display = "none"));
-      } else {
-        // Otherwise, show the target section and hide the other sections
-        showSection(targetSectionId);
-      }
-  
-      // Scroll to the top of the page
-      window.scrollTo(0, 0);
-    });
+  homeSection.style.display = "none"; // Hides the home section
+  otherSections.forEach((section) => {
+    if (section.id === sectionId) {
+      section.style.display = "block";
+    } else {
+      section.style.display = "none";
+    }
   });
+}
 
+// Displaying the home section by default
+homeSection.style.display = "block";
 
-  // Donate button click event
-  document.querySelector("#donate-btn-now").addEventListener("click", () => {
-    toggleSection("donation");
+//  click event listeners to the navigation links
+navLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault(); // Prevents reloading - the default link behavior
+
+    // Get the target section ID from the link's href
+    const targetSectionId = event.target.getAttribute("href").slice(1);
+
+    if (targetSectionId === "home-page") {
+      // If the target section is the home page, show the home section
+      homeSection.style.display = "block";
+      otherSections.forEach((section) => (section.style.display = "none"));
+    } else {
+      // Otherwise, show the target section and hide the other sections
+      showSection(targetSectionId);
+    }
+
+    // Scroll to the top of the page
+    window.scrollTo(0, 0);
   });
-//This code handles the navigation menu and toggles the visibility of sections.
+});
+
+// Event listeners for the "Donate" and "Donate Now" buttons
+
+const donateButton = document.getElementById("donate-btn-now");
+const donateLink = document.getElementById("donate-btn");
+
+// function to display donor form when donate buttons are clicked
+const handleDonateButtonClicks = () => {
+  showSection("donation");
+};
+
+donateButton.addEventListener("click", handleDonateButtonClicks);
+donateLink.addEventListener("click", handleDonateButtonClicks);
 
 //function to display the donor's Thank you card
 function showThankYou() {
@@ -73,46 +77,44 @@ function showThankYou() {
 }
 
 //function that posts data to db.json.(Andy)
-function postDetails(key){
+function postDetails(key) {
   const form = document.getElementById("foodForm");
-  
+
   //Post request to db.json file to specific key
-    fetch(`db.json/${key}`,{
-      method: "POST",
-      body: JSON.stringify({
-        firstName: form.firstName.value,
-        lastName: form.lastName.value,
-        contact: form.contact.value,
-        foodCategory: form.foodCategory.value,
-        description: form.description.value,
-        location: form.location.value,
-      }),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      }
-    } 
-    )
-    .then(response => {
-      if (!response.ok){
+  fetch(`db.json/${key}`, {
+    method: "POST",
+    body: JSON.stringify({
+      firstName: form.firstName.value,
+      lastName: form.lastName.value,
+      contact: form.contact.value,
+      foodCategory: form.foodCategory.value,
+      description: form.description.value,
+      location: form.location.value,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
         throw new Error(response.statusText);
       } else {
         return response.json();
       }
     })
-    .then (data => {
-      console.log("Data successfully posted", data)
+    .then((data) => {
+      console.log("Data successfully posted", data);
     })
-    .catch(error => {
+    .catch((error) => {
       console.log("Error posting data", error);
     });
-  
-};
-function handleDonorSubmission(){
-      postDetails("Donors");// post data to the donors key.
-      showThankYou(); // shows the thank you card.
-};
-function handleRecipientSubmission(event){
-  event.preventDefault();
-  postDetails("Recipients");// post data to the recipients key.
+}
+function handleDonorSubmission() {
+  postDetails("Donors"); // post data to the donors key.
   showThankYou(); // shows the thank you card.
-};
+}
+function handleRecipientSubmission(event) {
+  event.preventDefault();
+  postDetails("Recipients"); // post data to the recipients key.
+  showThankYou(); // shows the thank you card.
+}
